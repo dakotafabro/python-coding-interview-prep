7e8f5112813c61f85486ad389bf06636e912b06c
dakotafabro <dakotafabrocgu@gmail.com>
2022-01-02 20:19:04 -0800

    completed data and algo course

--- 06-BinarySearchTree/BST_Constructor.py
+++ 06-BinarySearchTree/BST_Constructor.py
@@ -44,11 +44,63 @@ class BinarySearchTree:
             current_node = current_node.left
         return current_node
 
+## Breadth First Search - Starting at the top and moving through all the rows of values
+## Depth First Search - TBD
 
-                
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
 
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
 
+    def dfs_pre_order(self):
+        results = []
         
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        
+        traverse(self.root) ## this kicks off the traverse function -- this is the starting place
+        return results
+
+    def dfs_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results
+
+    def dfs_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
+            
         
 my_tree = BinarySearchTree() ## creates the empty tree
 my_tree.insert(47)
@@ -59,7 +111,6 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.min_value_node(my_tree.root))
-print(my_tree.min_value_node(my_tree.root.right))
+print(my_tree.dfs_in_order())
 
 
